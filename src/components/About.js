e091136a918b40e87b2fe96fb3f614226f29e6d4
shiva kumar <nanishiva2022001@gmail.com>
2024-08-20 08:59:23 +0530

    Initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -8,11 +8,11 @@ const About = () => {
         <div className="about-text">
           <h2>About Me</h2>
           <p>
-            Hello! I'm Shiva Kumar Bandi, a Full Stack Web Developer specializing in the MERN stack.
-            With hands-on experience in building dynamic web applications, I am skilled in JavaScript,
-            Python, and frameworks like React and Node.js. Certified in MERN Stack Development, I bring
-            a problem-solving mindset, quick learning ability, and a passion for innovation. My background
-            in Electronics and Communication Engineering complements my technical expertise. In my free
+            Hello! I'm Shiva Kumar Bandi, a Full Stack Web Developer specializing in the MERN stack. 
+            With hands-on experience in building dynamic web applications, I am skilled in JavaScript, 
+            Python, and frameworks like React and Node.js. Certified in MERN Stack Development, I bring 
+            a problem-solving mindset, quick learning ability, and a passion for innovation. My background 
+            in Electronics and Communication Engineering complements my technical expertise. In my free 
             time, I enjoy puzzle-solving and playing the piano.
           </p>
         </div>
@@ -21,17 +21,8 @@ const About = () => {
         </div>
       </div>
 
-      {/* New Overview Section */}
-      <div className="overview-section">
-        <h3>Overview</h3>
-        <p>
-          I bring a unique blend of creativity and technical expertise to every project. I strive to build
-          applications that not only meet but exceed expectations in terms of performance, scalability, and
-          user experience.
-        </p>
-
-        {/* Icons Section */}
-        <div className="overview-icons">
+      {/* Icons Section */}
+      <div className="overview-icons">
           <div className="overview-icon">
             <i className="icon-react"></i>
             <h4>React Developer</h4>
@@ -49,7 +40,6 @@ const About = () => {
             <h4>MERN Stack Developer</h4>
           </div>
         </div>
-      </div>
     </section>
   );
 };
